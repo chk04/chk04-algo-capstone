879fbb574ede57b1525d92abeb5a5146fb2ffce5
chk04 <73590222+chk04@users.noreply.github.com>
2020-11-29 09:17:30 +0700

    Update app.py
    help edit

--- app.py
+++ app.py
@@ -2,11 +2,52 @@ from flask import Flask, request
 import pandas as pd 
 app = Flask(__name__) 
 
-@app.route('/api-help')
+@app.route('/help')
 def home():
     return '''
-            <p>halo nama saya choirul</p>
-            <p><br></p>
+        <p>Deskripsi: (Contoh Dokumentasi)</p>
+        <p>API ini dinominasikan sebagai capstone project yang berguna untuk mengirimkan data kepada user. Proses wrangling dilakukan sesuai endpoint-endpoint yang dimaksud. Base url dari aplikasi ini adalah https://chk04-capstone.herokuapp.com/</p>
+        <p><br></p>
+        <p>Endpoints:&nbsp;</p>
+        <p>1. / help, method = GET</p>
+        <p style="margin-left: 20px;">Merupakan endpoint home, dan akan mengembalikan nilai berupa string selamat datang</p>
+        <p style="margin-left: 20px;"><br></p>
+        <p>2. /data/&lt;data_name&gt;/&lt;column&gt;/&lt;value&gt;&apos;, methods=GET</p>
+        <p style="margin-left: 20px;">Mengambil data &lt;data_name&gt; dengan filter pada kolom &lt;column&gt; spesifik pada &lt;value&gt;.&nbsp;</p>
+        <p style="margin-left: 20px;"><span style='color: rgb(0, 0, 0); font-family: "Times New Roman"; font-size: medium; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;'>Return: .json</span>&nbsp;</p>
+        <p style="margin-left: 20px;">Data yang tersedia: rice.csv</p>
+        <p style="margin-left: 20px;">Coloum yang tersedia:<br>receipt_id, receipts_item_id, purchase_time category, sub_category, store, unit_price, discount, quantity, yearmonth</p>
+        <p style="margin-left: 20px;">contoh:&nbsp;</p>
+        <p style="margin-left: 20px;"><span style='color: rgb(0, 0, 0); font-family: "Times New Roman"; font-size: medium; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;'>a. https://chk04-capstone.herokuapp.com/data/rice.csv/store/minimarket</span></p>
+        <p style="margin-left: 20px;"><span style='color: rgb(0, 0, 0); font-family: "Times New Roman"; font-size: medium; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;'>b. https://chk04-capstone.herokuapp.com/data/rice.csv/store/supermarket</span></p>
+        <p>&nbsp; &nbsp;</p>
+        <p>3. /rice, <span style='color: rgb(0, 0, 0); font-family: "Times New Roman"; font-size: medium; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;'>methods=GET</span></p>
+        <p style="margin-left: 20px;"><span style='color: rgb(0, 0, 0); font-family: "Times New Roman"; font-size: medium; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;'>Mengambil data pada data rice.csv dengan</span></p>
+        <p style="margin-left: 20px;"><span style='color: rgb(0, 0, 0); font-family: "Times New Roman"; font-size: medium; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;'>Return: .json</span>&nbsp;</p>
+        <p style="margin-left: 20px;"><br></p>
+        <p>4. /rice/&lt;store&gt;, <span style='color: rgb(0, 0, 0); font-family: "Times New Roman"; font-size: medium; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;'>methods=GET</span>&nbsp;</p>
+        <p style="margin-left: 20px;">Mengambil data dari rice.csv dengan filter pada tipe penjualan &lt;store&gt; &nbsp;</p>
+        <p style="margin-left: 20px;"><span style='color: rgb(0, 0, 0); font-family: "Times New Roman"; font-size: medium; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;'>Return: .json</span>&nbsp;</p>
+        <p style="margin-left: 20px;">Data yang tersedia: rice.csv</p>
+        <p style="margin-left: 20px;">Tipe penjualan yang tersedia: minimarket, supermarket, hypermarket</p>
+        <p style="margin-left: 20px;">contoh:&nbsp;</p>
+        <p style="margin-left: 20px;"><span style='color: rgb(0, 0, 0); font-family: "Times New Roman"; font-size: medium; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;'>a. https://chk04-capstone.herokuapp.com/rice/minimarket</span></p>
+        <p style="margin-left: 20px;"><span style='color: rgb(0, 0, 0); font-family: "Times New Roman"; font-size: medium; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;'>b. https://chk04-capstone.herokuapp.com/rice/supermarket</span></p>
+        <p style="margin-left: 20px;"><span style='color: rgb(0, 0, 0); font-family: "Times New Roman"; font-size: medium; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;'>b. <a href="https://chk04-capstone.herokuapp.com/rice/hypermarket">https://chk04-capstone.herokuapp.com/rice/hypermarket</a></span></p>
+        <p style="margin-left: 20px;"><br></p>
+        <p><span style='color: rgb(0, 0, 0); font-family: "Times New Roman"; font-size: medium; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;'>5. /rice/purchasemonth/&lt;month&gt;&apos;, methods=GET</span></p>
+        <p style="margin-left: 20px;">Mengambil data dari rice.csv dengan filter pada bulan penjualan &lt;month&gt; &nbsp;</p>
+        <p style="margin-left: 20px;"><span style='color: rgb(0, 0, 0); font-family: "Times New Roman"; font-size: medium; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;'>Return: .json</span>&nbsp;</p>
+        <p style="margin-left: 20px;">Data yang tersedia: rice.csv</p>
+        <p style="margin-left: 20px;">contoh:&nbsp;</p>
+        <p style="margin-left: 20px;"><span style='color: rgb(0, 0, 0); font-family: "Times New Roman"; font-size: medium; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;'>a. <a data-fr-linked="true" href="https://chk04-capstone.herokuapp.com/rice/purchasemonth/November">https://chk04-capstone.herokuapp.com/rice/purchasemonth/November</a></span></p>
+        <p style="margin-left: 20px;"><br></p>
+        <p><span style='color: rgb(0, 0, 0); font-family: "Times New Roman"; font-size: medium; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;'>6. /rice/purchase, method=GET,PUSH</span></p>
+        <p style="margin-left: 20px;">Mengambil data dari rice.csv dengan filter pada tahun dan bulan penjualan, dengan GUI</p>
+        <p style="margin-left: 20px;">Return: .json</p>
+        <p style="margin-left: 20px;">url: httos://chk04-capstone.herokuapp.com/purchasedate</p>
+        <p style="margin-left: 20px;"><br></p>
+        <p style="margin-left: 20px;"><br></p>
     '''
 
 
@@ -42,7 +83,7 @@ def get_data_beras_mpurchase(month):
     data = data[mask]
     return (data.to_json())
 
-@app.route('/rice/purchasemonth', methods=['GET', 'POST']) #allow both GET and POST requests
+@app.route('/rice/purchasedate', methods=['GET', 'POST']) #allow both GET and POST requests
 def form():
     if request.method == 'POST':  # Hanya akan tampil setelah melakukan POST (submit) form
         key1 = 'year'
